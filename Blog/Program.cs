ab61d8e9cb9de1e441018233c1a2c5fa796adcee
Davy de Lima Carvalho <davy.carvalho@ambevtech.com.br>
2022-02-18 17:10:34 -0300

    complete JWT Auth with password generation

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -1,6 +1,27 @@
+using Blog;
 using Blog.Data;
+using Blog.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
 
 builder.Services.AddControllers()
     .ConfigureApiBehaviorOptions(options =>
@@ -8,8 +29,12 @@ builder.Services.AddControllers()
             options.SuppressModelStateInvalidFilter = true;
         });
 builder.Services.AddDbContext<BlogDataContext>();
+builder.Services.AddTransient<TokenService>();
 
 var app = builder.Build();
+
 app.MapControllers();
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();
---
+++ Blog/ViewsModels/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blog.ViewsModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Informe o Email")]
+        [EmailAddress(ErrorMessage = "E-mail inválido")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Informe a senha")]
+        public string Password { get; set; }
+    }
+}
